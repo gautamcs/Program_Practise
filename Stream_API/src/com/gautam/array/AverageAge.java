d5374821a7144b1d357c4ab5a0a56fbe91226d24
Gautam Kumar <gautam14cs@gmail.com>
2025-02-09 22:59:58 +0530

    Code Updated

---
+++ Stream_API/src/com/gautam/array/AverageAge.java
@@ -0,0 +1,11 @@
+package com.gautam.array;
+
+import java.util.Arrays;
+
+public class AverageAge {
+    public static void main(String[] args) {
+        int[] age = {23,32,11,45,54,67,54,35};
+
+        System.out.println(" Original Data: "+ Arrays.toString(age));
+    }
+}
