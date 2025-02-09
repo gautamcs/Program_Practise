0c40a2e13522616a015807fe4fc4ace083b51f34
Gautam Kumar <gautam14cs@gmail.com>
2025-02-09 22:52:21 +0530

    DSA Repo UPdate

---
+++ DSA/src/com/gautam/algo/sorting/SelectionSort.java
@@ -0,0 +1,11 @@
+package com.gautam.algo.sorting;
+
+import java.util.Arrays;
+
+public class SelectionSort {
+    public static void main(String[] args) {
+        int[] arr = {2,1,5,8,5,4,9,0,7,11};
+
+        System.out.println(" Original Array: "+ Arrays.toString(arr));
+    }
+}
---
+++ DSA/src/com/gautam/main/Data.java
@@ -0,0 +1,7 @@
+package com.gautam.main;
+
+public class Data {
+    public static void main(String[] args) {
+        System.out.println(" DSA ");
+    }
+}
