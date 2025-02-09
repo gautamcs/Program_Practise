e795a93f41a85dc21cb14b6bb0b9a0885f0c13bf
Gautam Kumar <gautam14cs@gmail.com>
2025-02-09 22:47:55 +0530

    DSA Repo

---
+++ DSA/src/com/gautam/algo/searching/BinarySearch.java
@@ -0,0 +1,12 @@
+package com.gautam.algo.searching;
+
+import java.util.Arrays;
+
+public class BinarySearch {
+    public static void main(String[] args) {
+
+        int[] arr = {1,3,5,6,7,11,15,21,32,56};
+
+        System.out.println(" Original Array Data: "+ Arrays.toString(arr));
+    }
+}
