32f56ac76c34c48d1c00f463c63bfeba32bf7a02
Gautam Kumar <gautam14cs@gmail.com>
2025-03-05 00:23:03 +0530

    Initial Commit-for SDE-Sheet

---
+++ SDE-Sheet/src/ZeroShift.java
@@ -0,0 +1,11 @@
+import java.util.Arrays;
+
+public class ZeroShift {
+    public static void main(String[] args) {
+
+        int[] arr = {1,0,1,0,0,1,1,0};
+
+        System.out.println(" Original Array: "+ Arrays.toString(arr));
+        
+    }
+}
