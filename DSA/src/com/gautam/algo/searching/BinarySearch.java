ecc398ef24217c7ed320da89d4e7210e70b09218
Gautam Kumar <gautam14cs@gmail.com>
2025-04-12 23:15:23 +0530

    Binary Search Changes

--- DSA/src/com/gautam/algo/searching/BinarySearch.java
+++ DSA/src/com/gautam/algo/searching/BinarySearch.java
@@ -6,7 +6,7 @@ public class BinarySearch {
     public static void main(String[] args) {
 
         int[] arr = {1,3,5,6,7,11,15,21,32,56};
-
+        int item = 21;
         System.out.println(" Original Array Data: "+ Arrays.toString(arr));
     }
 }
