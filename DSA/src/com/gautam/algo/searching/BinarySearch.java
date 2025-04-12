144feb9c17c14e778b89510581bc7c6c815db3a2
Gautam Kumar <gautam14cs@gmail.com>
2025-04-13 01:45:02 +0530

    Sorting method update

--- DSA/src/com/gautam/algo/searching/BinarySearch.java
+++ DSA/src/com/gautam/algo/searching/BinarySearch.java
@@ -6,7 +6,44 @@ public class BinarySearch {
     public static void main(String[] args) {
 
         int[] arr = {1,3,5,6,7,11,15,21,32,56};
-        int item = 21;
+        int item = 56;
         System.out.println(" Original Array Data: "+ Arrays.toString(arr));
+
+        //int index = binarySearchNormal(arr, 0, arr.length, item);
+        int index = binarySearchRecursive(arr, 0, arr.length, item);
+        System.out.println(" Index : "+index);
+    }
+
+    public static int binarySearchNormal(int arr[], int low, int high, int item) {
+
+        while(low < high) {
+            int mid = low + (high - low) / 2;
+            if (item == arr[mid]) {
+                System.out.println(" Item found at : "+mid);
+                return mid;
+            } else if (item <= arr[mid]) {
+                high = mid - 1;
+            } else {
+                low = mid + 1;
+            }
+        }
+        System.out.println(" Item Not Found! ");
+        return -1;
+    }
+
+
+    public static int binarySearchRecursive(int arr[], int low, int high, int item) {
+
+        if(low > high) {
+            return -1;
+        }
+        int mid = low + (high - low) / 2;
+        if(item == arr[mid]) {
+            return mid;
+        } else if(item < arr[mid]) {
+            return binarySearchRecursive(arr, low, mid-1, item);
+        } else {
+            return binarySearchRecursive(arr, mid+1, high, item);
+        }
     }
 }
--- DSA/src/com/gautam/algo/sorting/SelectionSort.java
+++ DSA/src/com/gautam/algo/sorting/SelectionSort.java
@@ -2,10 +2,38 @@ package com.gautam.algo.sorting;
 
 import java.util.Arrays;
 
+/*
+*   Selection Sort :
+*   Time Complexity: O(n)2
+*   Each pass select the smallest element and place it at exact position
+*   First we find the smallest element and put it at the 0'th index, then find the 2nd smallest element
+*   and put it at 1st index, and so on ...
+* */
 public class SelectionSort {
+
+    public static void selectionSortWay1(int arr[], int len) {
+        for(int i = 0; i < len; i++) {
+            int min = i;
+            for(int j = i; j < len; j++) {
+                // if(arr[min] > arr[j]) { // Increasing Order
+                if(arr[min] < arr[j]) { // Decreasing Order
+                    min = j;
+                }
+            }
+            if(min != i) {
+                int temp = arr[min];
+                arr[min] = arr[i];
+                arr[i] = temp;
+            }
+        }
+    }
     public static void main(String[] args) {
         int[] arr = {2,1,5,8,5,4,9,0,7,11};
 
         System.out.println(" Original Array: "+ Arrays.toString(arr));
+
+        selectionSortWay1(arr, arr.length);
+        System.out.println(" Sorted Array: "+ Arrays.toString(arr));
+
     }
 }
