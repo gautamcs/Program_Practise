541af43ce8ac1fa664cfd7ffce658ce82c963321
Gautam Kumar <gautam14cs@gmail.com>
2025-04-25 23:59:28 +0530

    QuickSort program update

--- DSA/src/com/gautam/algo/sorting/QuickSort.java
+++ DSA/src/com/gautam/algo/sorting/QuickSort.java
@@ -6,7 +6,9 @@ import java.util.Random;
 public class QuickSort {
     public static void quickSortImpl(int[] arr, int low, int high) {
         if(low < high) {
-            int pi = partition(arr, low, high);
+            //int pi = partitionIndexLast(arr, low, high);
+            //int pi = partitionIndexFirst(arr, low, high);
+            int pi = partitionIndexMiddle(arr, low, high);
             quickSortImpl(arr, low, pi-1);
             quickSortImpl(arr, pi+1, high);
         }
@@ -20,7 +22,8 @@ public class QuickSort {
         arr[high] = temp;
     }
 
-    public static int partition(int[] arr, int low, int high) {
+   // When the last index is selected as Pivot element
+    public static int partitionIndexLast(int[] arr, int low, int high) {
         int i = low-1;
         // random(arr, low, high); // Use when pivot element needs to be selected as random number
         int pi = arr[high];
@@ -34,13 +37,49 @@ public class QuickSort {
         return (i+1);
     }
 
+    // When the first element is selected as Pivot index
+    public static int partitionIndexFirst(int[] arr, int low, int high) {
+        int pi = arr[low];
+        int i = high;
+        for(int j = high; j >= low; j--) {
+            if(arr[j] > pi) {
+                swap(arr, i, j);
+                i--;
+            }
+        }
+        swap(arr, i, low);
+        return i;
+    }
+
+    public static int partitionIndexMiddle(int[] arr, int low, int high) {
+        int pi = low + (high-low) / 2;
+        int pivot = arr[pi];
+        int i = low + 1;
+        int j = high;
+
+        while (i <= j) {
+            while (i <= j && arr[i] < pivot)
+                i++;
+
+            while(i <= j && arr[j] > pivot)
+                j--;
+
+            if(i < j) {
+                swap(arr, i, j);
+                i++;
+                j--;
+            }
+        }
+        return j;
+    }
     public static void swap(int[] arr, int i, int j) {
         int temp = arr[i];
         arr[i] = arr[j];
         arr[j] = temp;
     }
     public static void main(String[] args) {
-        int[] arr = {10,30, 40, 90, 80};
+        //int[] arr = {10,30, 40, 90, 80};
+        int[] arr = {10, 30, 20, 50, 40, 90, 80, 70, 60};
         System.out.println(" Original Array: "+ Arrays.toString(arr));
         quickSortImpl(arr, 0, arr.length-1);
         System.out.println(" Sorted Array: "+Arrays.toString(arr));
